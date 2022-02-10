d0a8de43bff39f04713ba2e7abda51355922fb62
mahparhizkar <52155626+mahparhizkar@users.noreply.github.com>
2022-02-10 13:41:12 +0330

    Updated setup file

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ with open('LICENSE') as f:
 setup(
     name='sample',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='Car Damage Detection project is a machie learning project for detection of claim part in image.',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
+    author='Mahboub Parhizkar',
+    author_email='mah.parhizkar88@gmail.com',
     url='https://github.com/mahparhizkar/car-damage-detection',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
